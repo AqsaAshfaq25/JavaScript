7f61b7c4b637b186c4b0b7e55c7501a07481f164
AqsaAshfaq <113245995+AqsaAshfaq25@users.noreply.github.com>
2023-04-07 17:11:42 +0500

    First Commit

---
+++ AssignmentJS_1(SMIT)/assign.js
@@ -0,0 +1,141 @@
+// Q1) Write a JavaScript Program that accepts two numbers in two prompts 
+// and displays the larger one in the console?
+
+// let ValueA = prompt('Enter Value A');
+// let ValueB = prompt('Enter Value B'); 
+// let show = Math.max(ValueA, ValueB);
+
+// if(ValueA >= show){
+//     console.log("Value A =",ValueA,"is Greater.");
+// }else if(ValueB >= show){
+//     console.log("Value B =",ValueB,"is Greater.");
+// }else if(ValueA === ValueB){
+//     console.log("Both Value A =",ValueA, "And Value B =",ValueB,"Are Equal.");
+// }
+
+// Q2) Write a JavaScript conditional statement to find the sign of a number. 
+// Display an alert box with the specified sign?
+
+// let userInput = prompt('Enter Any Number');
+// if (userInput>0 ){
+//        alert("The sign is +");
+//        console.log("The sign is +");
+// }
+// else if (userInput<0){
+//     alert("The sign is -");
+//     console.log("The sign is -");
+// }
+
+// Q3) Write a JavaScript Program that accepts five numbers in five prompts 
+// and displays the larger one in the console?
+
+// let Number1 = prompt('Enter Value One');
+// let Number2 = prompt('Enter Value Two'); 
+// let Number3 = prompt('Enter Value Three'); 
+// let Number4 = prompt('Enter Value Four'); 
+// let Number5 = prompt('Enter Value Five'); 
+// let result = Math.max(Number1, Number2, Number3, Number4, Number5)
+
+// if(Number1 >=result && Number1 >=result && Number1 >=result && Number1 >=result){
+//     console.log("Value One =",Number1,"is Larger.");
+//     console.log("The values that you inserted are "+ Number1,Number2,Number3,Number4,Number5 );
+
+// }else if(Number2 >=result && Number2 >=result && Number2 >=result && Number2 >=result){
+//     console.log("Value Two =",Number2,"is Larger.");
+//     console.log("The values that you inserted are "+ Number1,Number2,Number3,Number4,Number5 );
+
+// }else if(Number3 >=result && Number3 >=result && Number3 >=result && Number3 >=result){
+//     console.log("Value Three =",Number3,"is Larger.");
+//     console.log("The values that you inserted are "+ Number1,Number2,Number3,Number4,Number5 );
+
+// }else if(Number4 >=result && Number4 >=result && Number4 >=result && Number4 >=result){
+//     console.log("Value Four =",Number4,"is Larger.");
+//     console.log("The values that you inserted are "+ Number1,Number2,Number3,Number4,Number5 );
+// }
+// else
+// {
+//     console.log("Value Five =",Number5,"is Larger.");
+//     console.log("The values that you inserted are "+ Number1,Number2,Number3,Number4,Number5 );
+// }
+
+// Q4) Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if 
+// the current number is odd or even , and display a message to the screen?
+
+// for (let a=0; a<=15; a++) {
+//     if (a === 0) {
+//         document.write(a +  " is even <br>");
+//         console.log(a +  " is even");
+//     }
+//     else if (a % 2 === 0) {
+//         document.write(a +  " is even <br>");
+//         console.log(a + " is even");   
+//     }
+//     else {
+//         document.write(a +  " is odd <br>");
+//         console.log(a + " is odd");
+//     }
+// }
+
+// Q5) Write a JavaScript Program which computes the average marks of the following students Then, 
+// this average is used to determine the corresponding grade?
+
+let studentsMarks = prompt("Enter Your Number To Find a Grade:");
+
+let AvgMarks = 1;
+
+for (var i=0; i <studentsMarks.length; i++) {
+        AvgMarks += studentsMarks[i];
+        var avg = (AvgMarks/studentsMarks.length);
+}
+
+console.log(`Average Marks: ${(AvgMarks) / studentsMarks.length}`);
+
+        if (avg < 60){
+          console.log("Grade : F");      
+          } 
+        else if (avg < 70) {
+                console.log("Grade : D"); 
+                  } 
+        else if (avg < 80) 
+             {
+                console.log("Grade : C"); 
+        } else if (avg < 90) {
+                console.log("Grade : B"); 
+        } else if (avg < 100) {
+                console.log("Grade : A");    
+}
+
+// Q6) Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three 
+// print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which 
+// are multiples of both three and five print "FizzBuzz"?
+
+// let n = 100
+// for (let i=1; i<= n; i++)
+// {
+//     if (i % 5 == 0 && i % 3 == 0)
+//         console.log("FizzBuzz");
+//     else if (i % 3 == 0)
+//         console.log("Fizz");
+//     else if (i % 5 == 0)
+//         console.log("Buzz");
+//     else
+//         console.log(i + " ");
+// }
+
+// Q7) Write a JavaScript program to construct the following pattern , Using a nested for loop?
+
+// document.write("<br>");
+//  //  Star Pattern:  
+//  for(let a = 0; a<6; a++){
+
+//     for(let b = 0; b<=1*a; b++){
+//     document.write("*");
+//     }
+//      document.write("<br>");
+//    }
+ 
+//    document.write("<br>");
+
+
+
+

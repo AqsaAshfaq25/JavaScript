2dbcd4910ed74d5f088a41e06fde6ecb0159b6e3
AqsaAshfaq <aqsaashfaq510@gmail.com>
2023-04-07 18:46:13 +0500

    Update

--- AssignmentJS_1(SMIT)/assign.js
+++ AssignmentJS_1(SMIT)/assign.js
@@ -102,30 +102,6 @@
 //     console.log("Your Grade : A");    
 // }
 
-// let AvgMarks = 1;
-
-// for (var i=0; i <studentsMarks.length; i++) {
-//         AvgMarks += studentsMarks[i];
-//         var average = (AvgMarks/studentsMarks.length);
-// }
-
-// console.log(`Average Marks: ${(AvgMarks) / studentsMarks.length}`);
-
-//         if (average < 60){
-//           console.log("Grade : F");      
-//           } 
-//         else if (average < 70) {
-//                 console.log("Grade : D"); 
-//                   } 
-//         else if (average < 80) 
-//              {
-//                 console.log("Grade : C"); 
-//         } else if (average < 90) {
-//                 console.log("Grade : B"); 
-//         } else if (average < 100) {
-//                 console.log("Grade : A");    
-// }
-
 // Q6) Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three 
 // print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which 
 // are multiples of both three and five print "FizzBuzz"?
